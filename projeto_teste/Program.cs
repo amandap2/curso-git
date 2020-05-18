72852893d73e7bc5dc3bb2573a60e3a53b9d197e
Amanda Paim <amandarodriguespaim@gmail.com>
2020-05-18 13:46:24 -0300

    Acrescentada a mensagem desejando um bom dia

--- projeto_teste/Program.cs
+++ projeto_teste/Program.cs
@@ -8,6 +8,8 @@ namespace projeto_teste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Tenha um bom dia");
+            Console.WriteLine();
         }
     }
 }
